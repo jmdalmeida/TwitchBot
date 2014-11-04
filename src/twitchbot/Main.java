385b12b4356f8c9272efbe2cfa9d0e58c90be4b1
João Almeida <jmdalmeida92@gmail.com>
2014-11-04 02:29:21 +0000

    2

--- src/twitchbot/Main.java
+++ src/twitchbot/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static void main(String[] args) throws IrcException, IOException {
         TwitchBot bot = new TwitchBot("dieselownsu");
-        //DieselTheBot bot = new DieselTheBot("playhearthstone");
     }
 
 }
